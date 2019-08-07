85bec6230df938923467653c26c0cf54ec6b7f12
elifkendaz <elifkendaz@yahoo.com.tr>
2019-08-07 14:37:23 +0300

    ilk commit

---
+++ baglistesi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Elif
+ */
+public class baglistesi {
+    
+     private int id;
+    private String bag;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBag() {
+        return bag;
+    }
+
+    public void setBag(String bag) {
+        this.bag = bag;
+    }
+
+    public baglistesi(int id, String bag) {
+        this.id = id;
+        this.bag = bag;
+    }
+    
+    
+    
+    
+}
